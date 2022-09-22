20486ffe9be4e8872aa1683c6d48b4533f46bf1b
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-22 06:03:12 +0300

    Starter files

---
+++ src/Config/Firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDGtiRacv9sC3y2C-CS3ej7nWH1UBHKhMM',
+  authDomain: 'port-2f6a5.firebaseapp.com',
+  projectId: 'port-2f6a5',
+  storageBucket: 'port-2f6a5.appspot.com',
+  messagingSenderId: '1006406399600',
+  appId: '1:1006406399600:web:38b5db1c6e0af13f0a66b5'
+};
+
+
+const app = initializeApp(firebaseConfig);

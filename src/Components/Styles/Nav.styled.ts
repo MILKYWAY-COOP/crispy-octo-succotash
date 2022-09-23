ace19fa0e1700c5b28ca91b173ec3dc896976b94
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-23 07:49:47 +0300

    PP

--- src/Components/Styles/Nav.styled.ts
+++ src/Components/Styles/Nav.styled.ts
@@ -26,45 +26,8 @@ export const StyledNav = styled.div`
   }
 
   .icons {
-    display: flex;
-    align-items: center;
-    gap: 1rem;
-
-    .active {
-      transition: 0.3s ease-in-out;
-      transform: scale(1.3);
-    }
-
-    #light,
-    #blue,
-    #purple,
-    #green {
-      height: 2em;
-      width: 2em;
-      border-radius: 50%;
-      border: 2px solid black;
-
-      &:hover {
-        transition: 0.3s ease;
-        cursor: pointer;
-        transform: scale(0.9);
-      }
-    }
-
-    #light {
-      background-color: #eaeaea;
-    }
-
-    #blue {
-      background-color: #152028;
-    }
-
-    #purple {
-      background-color: #463445;
-    }
-
-    #green {
-      background-color: #606856;
+    @media (max-width: 768px) {
+      display: none;
     }
   }
 

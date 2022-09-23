8647f17fed9675a05402ba79131606f1e2e9af9c
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-23 04:15:11 +0300

    Refactor

--- src/Components/Styles/App.styled.ts
+++ src/Components/Styles/App.styled.ts
@@ -1,15 +1,18 @@
 import styled from 'styled-components';
 
 const AppStyles = styled.div`
-  background: ${(props: any) => props.theme.mainColor};
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-size: cover;
-  color: hsl(192, 100%, 9%);
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  grid-template-rows: 15% 60% 20%;
+
   font-family: 'Syne Mono', monospace;
   font-size: 1.15em;
-  margin: 0;
-  transition: all 0.3s ease;
+  color: ${(props: any) => props.theme.mainTextColor};
+
+  & > * {
+    height: 100%;
+  }
 `;
 
 export default AppStyles;

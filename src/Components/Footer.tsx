a72d2207e7d2fa695320f1b53e41dfd9560efb40
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-23 07:49:16 +0300

    PP

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -4,28 +4,47 @@ import {
   FaSass,
   FaPython,
   FaReact,
-  FaJs
+  FaJs,
+  FaWordpress
 } from 'react-icons/fa';
 import { SiTypescript, SiFirebase } from 'react-icons/si';
-
 import { FooterStyled } from './Styles/Footer.styled';
+import Theme from './Theme';
 
-export const Footer = () => {
+
+export const Footer = (props: any) => {
+  const { setTheme, lightTheme, blueTheme, purpleTheme, greenTheme, theme } =
+    props;
+  
   return (
     <FooterStyled>
+      <div className='theme'>
+        <Theme
+          theme={theme}
+          setTheme={setTheme}
+          lightTheme={lightTheme}
+          purpleTheme={purpleTheme}
+          blueTheme={blueTheme}
+          greenTheme={greenTheme}
+        />
+      </div>
       <div className='icons'>
         <FaHtml5 />
         <FaCss3 />
         <FaSass />
         <SiFirebase />
-        <SiTypescript />
-        <FaReact />
+        <FaWordpress />
         <FaPython />
+        <FaReact />
         <FaJs />
+        <SiTypescript />
       </div>
       <div className='rights'>
         <p>
-          &copy;2022. A <a href='https://milkyway-coop.github.io/' target="_blank">MILKYWAY</a>{' '}
+          &copy;2022. A{' '}
+          <a href='https://milkyway-coop.github.io/' target='_blank'>
+            MILKYWAY
+          </a>{' '}
           PRODUCTION. All rights reserved
         </p>
       </div>

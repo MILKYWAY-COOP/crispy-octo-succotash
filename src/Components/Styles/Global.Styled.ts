96ed7b2b6ebee6840886c1a5b0561d06a6b9d711
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-23 04:15:15 +0300

    Refactor

--- src/Components/Styles/Global.Styled.ts
+++ src/Components/Styles/Global.Styled.ts
@@ -1,25 +1,16 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Syne+Mono&display=swap');
+
 * {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
-    
+    box-sizing: border-box;   
 }
+
 body {
-    color: hsl(192, 100%, 9%);
-    font-family: 'Syne Mono', monospace;
-    font-size: 1.15em;
-    margin: 0;
-}
-p {
-    opacity: 0.6;
-    line-height: 1.5;
-}
-img {
-    max-width: 100%;
+    background-color: ${(props: any) => props.theme.secondaryColor};
+    height: 100vh;
 }
 `;
 

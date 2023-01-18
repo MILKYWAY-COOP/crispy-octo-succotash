a7874f9b9e92ca4f833be0662815e40274fcf7c1
Stephen Muchendu <muchendumaina66@gmail.com>
2023-01-18 16:59:42 +0300

    Added dynamic year feature

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -11,11 +11,12 @@ import { SiTypescript, SiFirebase } from 'react-icons/si';
 import { FooterStyled } from './Styles/Footer.styled';
 import Theme from './Theme';
 
-
 export const Footer = (props: any) => {
   const { setTheme, lightTheme, blueTheme, purpleTheme, greenTheme, theme } =
     props;
-  
+
+  const year = new Date().getFullYear();
+
   return (
     <FooterStyled>
       <div className='theme'>
@@ -29,19 +30,19 @@ export const Footer = (props: any) => {
         />
       </div>
       <div className='icons'>
+        <FaWordpress />
+        <SiFirebase />
         <FaHtml5 />
         <FaCss3 />
         <FaSass />
-        <SiFirebase />
-        <FaWordpress />
-        <FaPython />
         <FaReact />
         <FaJs />
         <SiTypescript />
+        <FaPython />
       </div>
       <div className='rights'>
         <p>
-          &copy;2022. A{' '}
+          &copy;{year}. A{' '}
           <a
             href='https://milkyway-coop.github.io/'
             target='_blank'
